112d588047e053bf8718b879748279cd767d0ce2
Den4200 <dennis@dennispham.me>
2022-10-24 22:29:11 -0400

    Use actual package version for user agent.

--- lithium/lithium.py
+++ lithium/lithium.py
@@ -1,4 +1,5 @@
 import asyncio
+from importlib.metadata import version
 from os import PathLike
 from pathlib import Path
 from types import TracebackType
@@ -10,6 +11,7 @@ from httpx import AsyncClient
 from more_itertools import chunked, collapse
 
 SIGMA_ALDRICH_CAS_NUMBER_SEARCH_QUERY = "query ProductSearch($searchTerm: String, $page: Int!, $sort: Sort, $group: ProductSearchGroup, $selectedFacets: [FacetInput!], $type: ProductSearchType, $catalogType: CatalogType, $orgId: String, $region: String, $facetSet: [String]) {\n  getProductSearchResults(input: {searchTerm: $searchTerm, pagination: {page: $page}, sort: $sort, group: $group, facets: $selectedFacets, type: $type, catalogType: $catalogType, orgId: $orgId, region: $region, facetSet: $facetSet}) {\n    ...ProductSearchFields\n    __typename\n  }\n}\n\nfragment ProductSearchFields on ProductSearchResults {\n  metadata {\n    itemCount\n    setsCount\n    page\n    perPage\n    numPages\n    redirect\n    __typename\n  }\n  items {\n    ... on Substance {\n      ...SubstanceFields\n      __typename\n    }\n    ... on Product {\n      ...SubstanceProductFields\n      __typename\n    }\n    __typename\n  }\n  facets {\n    key\n    numToDisplay\n    isHidden\n    isCollapsed\n    multiSelect\n    prefix\n    options {\n      value\n      count\n      __typename\n    }\n    __typename\n  }\n  didYouMeanTerms {\n    term\n    count\n    __typename\n  }\n  __typename\n}\n\nfragment SubstanceFields on Substance {\n  _id\n  id\n  name\n  synonyms\n  empiricalFormula\n  linearFormula\n  molecularWeight\n  aliases {\n    key\n    label\n    value\n    __typename\n  }\n  images {\n    sequence\n    altText\n    smallUrl\n    mediumUrl\n    largeUrl\n    brandKey\n    productKey\n    label\n    __typename\n  }\n  casNumber\n  products {\n    ...SubstanceProductFields\n    __typename\n  }\n  match_fields\n  __typename\n}\n\nfragment SubstanceProductFields on Product {\n  name\n  productNumber\n  productKey\n  isSial\n  isMarketplace\n  marketplaceSellerId\n  marketplaceOfferId\n  cardCategory\n  cardAttribute {\n    citationCount\n    application\n    __typename\n  }\n  substance {\n    id\n    __typename\n  }\n  casNumber\n  attributes {\n    key\n    label\n    values\n    __typename\n  }\n  speciesReactivity\n  brand {\n    key\n    erpKey\n    name\n    color\n    __typename\n  }\n  images {\n    altText\n    smallUrl\n    mediumUrl\n    largeUrl\n    __typename\n  }\n  description\n  sdsLanguages\n  sdsPnoKey\n  similarity\n  paMessage\n  features\n  catalogId\n  materialIds\n  __typename\n}\n"  # noqa: E501
+USER_AGENT = f"Lithium {version('lithium')}"
 
 
 class Lithium:
@@ -34,7 +36,7 @@ class Lithium:
 
     async def get_cas_numbers(self):
         res = await self.httpx.get(
-            "https://en.wikipedia.org/wiki/List_of_CAS_numbers_by_chemical_compound"
+            "https://en.wikipedia.org/wiki/List_of_CAS_numbers_by_chemical_compound",
         )
         soup = BeautifulSoup(res.text, "html.parser")
 
@@ -48,7 +50,7 @@ class Lithium:
     async def search_cas_number(self, cas_number: str):
         res = await self.httpx.post(
             "https://www.sigmaaldrich.com/api",
-            headers={"User-Agent": "Lithium 0.1.0", "x-gql-country": "US", "x-gql-language": "en"},
+            headers={"User-Agent": USER_AGENT, "x-gql-country": "US", "x-gql-language": "en"},
             json={
                 "operationName": "ProductSearch",
                 "query": SIGMA_ALDRICH_CAS_NUMBER_SEARCH_QUERY,
@@ -78,7 +80,7 @@ class Lithium:
         async with self.httpx.stream(
             "GET",
             f"https://www.sigmaaldrich.com/US/en/sds/{sds['brand']}/{sds['product_number']}",
-            headers={"User-Agent": "Lithium 0.1.0"},
+            headers={"User-Agent": USER_AGENT},
             timeout=self.timeout,
         ) as res:
             async with aiofiles.open(output, "wb") as f:
